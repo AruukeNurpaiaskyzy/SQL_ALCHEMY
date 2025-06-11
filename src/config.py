da738cd6dcf5e1c91f6aa4b7dee01eb5e0b0e0f7
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-06-11 15:28:51 +0600

    added config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
-from pydentic_settings import BaseSettings, SettingsConfigDict
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 
 
@@ -11,10 +13,11 @@ class Settings(BaseSettings):
 
     @property
     def DATABASE_URL_asyncpg(self):
-      return f "postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}:{self.DB_NAME}:
+      return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
     @property
     def DATABASE_URL_psycopg(self):
-      return f "postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}:{self.DB_NAME}:
+      return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
     model_config = SettingsConfigDict(env_file = ".env")
 
