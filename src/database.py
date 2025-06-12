8b4b4f3177d91c4852d53975d9e14e78087ca678
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-06-12 22:20:45 +0600

    added database url

--- src/database.py
+++ src/database.py
@@ -3,5 +3,11 @@ from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import URL, create_engine, text
 from config import settings
 engine = create_engine(
-    url = 
+    url = settings.DATABASE_URL_psycopg,
+    echo =True,
+    pool_size = 5,
+    max_overflow=10
+
 )
+
+
