b659bc2b0f97c7befb338f8c970db34e90f82638
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-06-13 22:48:39 +0600

    connection

--- src/config.py
+++ src/config.py
@@ -1,24 +1,24 @@
-
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class Settings(BaseSettings):
     DB_HOST: str
-    DB_POST: int
+    DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
 
     @property
     def DATABASE_URL_asyncpg(self):
-      return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     @property
     def DATABASE_URL_psycopg(self):
-      return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-    
-    model_config = SettingsConfigDict(env_file = ".env")
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-settings =  Settings()
+    model_config = SettingsConfigDict(env_file=os.path.join(BASE_DIR, '.env'))
+
+settings = Settings()
+print("Loaded settings:", settings.model_dump())
--- src/database.py
+++ src/database.py
@@ -5,9 +5,12 @@ from config import settings
 engine = create_engine(
     url = settings.DATABASE_URL_psycopg,
     echo =True,
-    pool_size = 5,
-    max_overflow=10
+    # pool_size = 5,
+    # max_overflow=10,
 
 )
+with engine.connect() as conn:
+    res = conn.execute(text("SELECT VERSION()"))
+    print(f"{res.all()=}")
 
 
